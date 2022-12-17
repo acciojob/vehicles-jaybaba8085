b79379f3ce37302f1d376007c8006ca7fb7ba17a
SHARGINJAY PATEL <sharginjaypatel12021999@gmail.com>
2022-12-17 14:42:05 +0530

    done

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,10 +4,13 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+        super("name",0);
+        setManual(isManual);
+
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        int newSpeed = getCurrentSpeed() + rate ; //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -19,12 +22,37 @@ public class F1 extends Car {
          */
 
         if(newSpeed == 0) {
-            //Stop the car, set gear as 1
+            setGears(1);
+            stop();
+
+        }
+        if(newSpeed > 0 && newSpeed<=50) {
+            setCurrentSpeed(newSpeed);
+            setGears(1);
+        }
+        if(newSpeed > 50 && newSpeed<=100) {
+            setCurrentSpeed(newSpeed);
+            setGears(2);
+        }
+        if(newSpeed > 100 && newSpeed<=150) {
+            setCurrentSpeed(newSpeed);
+            setGears(3);
+        }
+        if(newSpeed > 150 && newSpeed<=200) {
+            setCurrentSpeed(newSpeed);
+            setGears(4);
+        }
+        if(newSpeed > 200 && newSpeed<=250) {
+            setCurrentSpeed(newSpeed);
+            setGears(5);
+        }
+        if(newSpeed > 250 ) {
+            setCurrentSpeed(newSpeed);
+            setGears(6);
         }
         //for all other cases, change the gear accordingly
-
-        if(newSpeed > 0) {
-            changeSpeed(newSpeed, getCurrentDirection());
-        }
+//        if(newSpeed > 0) {
+//            changeSpeed(newSpeed, steer());
+//        }
     }
 }
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,8 +3,11 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         F1 mercedes = new F1("mercedes", false);
-        mercedes.move(40, 0);
-        mercedes.accelerate(20);
+        mercedes.move(60, 0);
         mercedes.accelerate(-60);
+        mercedes.accelerate(0);
+//        Boat b=new Boat();
+//        b.setName("MyBoat");
+//       System.out.print(b.getVehicleName());
     }
 }

7a6fe5bce18526717f69050a0c344cf362d85167
SHARGINJAY PATEL <sharginjaypatel12021999@gmail.com>
2022-12-17 19:16:55 +0530

    checkk

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -17,7 +17,7 @@ public class Vehicle {
     public void steer(int direction){
 
         //direction is in degrees, adding it to the current direction
-        this.currentDirection = direction;
+        this.currentDirection  =this.currentDirection + direction;
         System.out.println("steer method called - The direction is changed to: " + currentDirection + " degrees");
     }
 
